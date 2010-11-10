928cbaf9eff4f658e686c9ff4f539774a8e41fb3
Tyler Gillies <tjgillies@gmail.com>
2010-11-10 06:40:20 +0000

    First Commit (hello world)

---
+++ account.rb
@@ -0,0 +1,55 @@
+require 'rubygems'
+require 'ezcrypto'
+require 'ezsig'
+require 'uuid'
+require 'typhoeus'
+require 'json'
+require 'open-uri'
+require 'differ'
+require 'ezcrypto'
+require 'yaml'
+require 'ap'
+@signer = EzCrypto::Signer.from_file('keys/key.priv')
+@verifier = @signer.verifier()
+mykey = @signer.public_key().to_s()
+credit = {}
+address_book = YAML.load_file('address.yaml')
+debt = {}
+keyring = []
+all_docs = open("http://tyler.couchone.com/couchcash/_all_docs")
+records = all_docs.read()
+json_records = JSON.parse(records)
+json_records["rows"].each() do |record|
+  next if record["id"] =~ /_design/
+  document = open("http://tyler.couchone.com/couchcash/#{record["id"]}").read()
+  json_document = JSON.parse(document)
+  keyring << json_document["ower_key"]
+  keyring << json_document["owed_key"]
+  if  mykey =~ /#{json_document["ower_key"]}/
+    puts "foo"
+  end
+  if json_document["ower_key"] == mykey
+    sig = json_document["sig"].to_a().pack("H*")
+    verified = @verifier.verify(sig,json_document["_id"])
+    if !verified
+      next
+    end
+    debt[json_document["owed_key"]] ||= 0
+    debt[json_document["owed_key"]] += json_document["amount"].to_i()
+  end
+  if json_document["owed_key"] == mykey
+    credit[json_document["ower_key"]] ||= 0
+    credit[json_document["ower_key"]] += json_document["amount"].to_i()
+  end
+  
+end
+debt.each_pair() do |owed,amount|
+  person = address_book.select{|person| person["public_key"].gsub(" ","") == owed.gsub(" ","") }
+  if person.empty?
+    name = owed
+  else
+    name = person.first()["name"]
+    paypal = person.first()["paypal"]
+  end
+  puts "You owe #{name} (#{paypal}) #{amount}"
+end
---
+++ owe.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'ezcrypto'
+require 'ezsig'
+require 'uuid'
+require 'typhoeus'
+require 'json'
+require 'yaml'
+
+@uuid = UUID.new
+@signer = EzCrypto::Signer.from_file('keys/key.priv')
+address_book = YAML.load_file('address.yaml')
+owed_name = ARGV[0]
+person = address_book.select{|person| person["name"] == owed_name }.first()
+if person.nil?()
+  abort("Person not in address book")
+end
+uuid = @uuid.generate()
+puts uuid
+sig = @signer.sign(uuid)
+public_key = @signer.public_key()
+owed_key = person["public_key"]
+ower_key = public_key
+amount = ARGV[1]
+sig_hex = sig.unpack("H*").to_s()
+puts sig_hex
+sig_bin = sig_hex.to_a.pack("H*")
+body = { :ower_key => ower_key, :owed_key => owed_key, :amount => amount, :sig => sig_hex }.to_json
+@response = Typhoeus::Request.put("http://tyler.couchone.com/couchcash/#{uuid}", :body => body, :headers => { :content_type => "application/json" })
+puts @response.body()
